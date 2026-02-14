11cede086a3dbd6d64e08eb7cfbbedfd67ba1073
MichaelSukkiSong <sa77irak@gmail.com>
2026-02-14 16:53:26 +0900

    change code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unused_imports)]
-use std::{io::Write, net::{TcpListener, TcpStream}};
+use std::{io::{BufReader, Write}, net::{TcpListener, TcpStream}};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -15,7 +15,6 @@ fn main() {
                 println!("accepted new connection");
 
                 handle_connection(&mut stream);
-
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -25,8 +24,9 @@ fn main() {
 }
 
 fn handle_connection(stream: &mut TcpStream) {
-
     loop {
+        let mut reader = BufReader::new(&mut *stream);
+
         stream.write_all(b"+PONG\r\n").unwrap();
     }
 }
