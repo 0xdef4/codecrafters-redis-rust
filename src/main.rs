4e1f5cc1dffbaf7e452695b400ef94eddb3def8f
MichaelSukkiSong <sa77irak@gmail.com>
2026-02-17 16:03:03 +0900

    Handle concurrent clients : minimum

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-#![allow(unused_imports)]
-use std::{io::{BufRead, BufReader, Read, Write}, net::{TcpListener, TcpStream}};
+// #![allow(unused_imports)]
+use std::{
+    io::{BufReader, Read, Write},
+    net::{TcpListener, TcpStream},
+    thread,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
 
-                handle_connection(&mut stream);
+                let _ = thread::spawn(move || {
+                    handle_connection(&mut stream);
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -30,8 +36,8 @@ fn handle_connection(stream: &mut TcpStream) {
             Ok(0) => break,
             Ok(_) => {
                 reader.get_mut().write_all(b"+PONG\r\n").unwrap();
-            },
-            Err(_) => break
+            }
+            Err(_) => break,
         }
     }
 }
