3011e83dcba7094dfa4531a4486404a4b9dee8f0
MichaelSukkiSong <sa77irak@gmail.com>
2026-03-29 15:51:10 +0900

    added bufreader

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unused_imports)]
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, BufReader, AsyncBufReadExt};
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
@@ -22,9 +22,9 @@ async fn main() {
 
 async fn handle_stream(stream: TcpStream) {
     let mut buffer = [0; 512];
-    let (mut rd, mut wr) = stream.into_split();
-    // let mut rd = BufReader::new(rd);
-    
+    let (rd, mut wr) = stream.into_split();
+    let mut rd = BufReader::new(rd);
+
     loop {
         match rd.read(&mut buffer).await {
             Ok(0) => break,
