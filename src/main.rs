efc91eeda67d00111a37031d9ca6cf6803d271e8
MichaelSukkiSong <sa77irak@gmail.com>
2026-03-30 20:20:49 +0900

    RESP parser

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unused_imports)]
-use tokio::io::{AsyncReadExt, AsyncWriteExt, BufReader, AsyncBufReadExt};
+use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader};
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
@@ -24,53 +24,53 @@ async fn handle_stream(stream: TcpStream) {
     let (rd, mut wr) = stream.into_split();
     let mut rd = BufReader::new(rd);
 
-    let mut buf = String::new();
-    
+    let mut buf = [0u8; 512];
+
     loop {
-        match rd.read_to_string(&mut buf).await {
-                Ok(0) => break,
-                Ok(_) => {
-                    println!("buf: {:?}", buf);
+        match rd.read(&mut buf).await {
+            Ok(0) => break,
+            Ok(n) => {
+                let received = String::from_utf8_lossy(&buf[..n]);
+                println!("received: {:?}", received);
 
-                    let resp_array = decode_arrays(&buf);
+                let resp_array = decode_arrays(&received);
 
-                    match resp_array.as_slice() {
-                        [cmd] if cmd.to_uppercase() == "PING".to_string() => {
-                            let _ = wr.write_all(b"+PONG\r\n").await;
-                        },
-                        [cmd, arg] if cmd.to_uppercase() == "ECHO".to_string() => {
-                            let response = encode_bulk_strings(arg.clone());
-                            let _ = wr.write_all(response.as_bytes()).await;
-                        }
-                        _ => todo!()
+                match resp_array.as_slice() {
+                    [cmd] if cmd.to_uppercase() == "PING".to_string() => {
+                        let _ = wr.write_all(b"+PONG\r\n").await;
                     }
-
+                    [cmd, arg] if cmd.to_uppercase() == "ECHO".to_string() => {
+                        let response = encode_bulk_strings(arg.clone());
+                        let _ = wr.write_all(response.as_bytes()).await;
+                    }
+                    _ => todo!(),
                 }
-                Err(_) => break,
+            }
+            Err(_) => break,
         }
     }
 }
 
 /// RESP encode bulk strings
-/// 
+///
 /// ```text
 /// $<length>\r\n<data>\r\n
 /// ```
-/// 
+///
 /// $ : The dollar sign ($) as the first byte.
-/// 
+///
 /// `<length>` : One or more decimal digits (0..9) as the string's length, in bytes, as an unsigned, base-10 value.
-/// 
+///
 /// \r\n : The CRLF terminator.
-/// 
+///
 /// `<data>` : The data.
-/// 
+///
 /// \r\n : A final CRLF.
-/// 
+///
 /// # Examples
-/// 
+///
 /// So the string "hello" is encoded as follows:
-/// 
+///
 /// ```text
 /// $5\r\nhello\r\n
 /// ```
@@ -84,23 +84,23 @@ fn decode_bulk_strings(input: String) -> String {
 }
 
 /// RESP encode arrays'
-/// 
+///
 /// ```text
 /// *<number-of-elements>\r\n<element-1>...<element-n>
 /// ```
-/// 
+///
 /// * : An asterisk (*) as the first byte.
-/// 
+///
 /// `<number-of-elements>` : One or more decimal digits (0..9) as the number of elements in the array as an unsigned, base-10 value.
-/// 
+///
 /// \r\n : The CRLF terminator.
-/// 
+///
 /// `<element-1>...<element-n>` : An additional RESP type for every element of the array.
-/// 
+///
 /// # Examples
-/// 
+///
 /// So the encoding of an array consisting of the two bulk strings "hello" and "world" is:
-/// 
+///
 /// ```text
 /// *2\r\n$5\r\nhello\r\n$5\r\nworld\r\n
 /// ```
@@ -119,23 +119,11 @@ fn encode_arrays(arr: &[&str]) -> String {
 
 /// RESP decode arrays'
 fn decode_arrays(input: &str) -> Vec<String> {
-    let mut output = Vec::new();
-    
     // Objective : *2\r\n $4\r\nECHO\r\n$3\r\nhey\r\n     ->     ["ECHO", "hey"]
-    let index_end_of_array_length = input.find(' ').unwrap();
-    let (arr_len, elements)= input.split_at(index_end_of_array_length);
 
-    // parse elements
-    // presupposition : assuming only $'s exist in 'elements'
-    // split with $
-    let mut v = elements.split('$').collect::<Vec<&str>>(); // $4\r\nECHO\r\n$3\r\nhey\r\n -> ["", "4\r\nECHO\r\n", "3\r\nhey\r\n"]
-    let v2 = v.split_off(1); // ["4\r\nECHO\r\n", "3\r\nhey\r\n"]
-
-    for el in v2 {
-        let element = el.split_ascii_whitespace().nth(1).unwrap();
-        output.push(element.to_string());
-    }
-
-    output
+    input.split("\r\n").filter(|e| !e.is_empty())
+    .filter(|e| e.starts_with('*'))
+    .filter(|e| e.starts_with('$'))
+    .map(|e| e.to_string())
+    .collect::<Vec<_>>()
 }
-
