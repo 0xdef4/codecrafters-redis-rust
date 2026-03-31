c24d82bd483a0fcb5eb58380802c48164b4ed1b8
MichaelSukkiSong <sa77irak@gmail.com>
2026-03-31 15:56:56 +0900

    test : Implement the SET & GET commands

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,26 @@
 use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader};
 use tokio::net::{TcpListener, TcpStream};
 
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+type Db = Arc<Mutex<HashMap<String, String>>>;
+
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
 
     loop {
         match listener.accept().await {
             Ok((stream, _addr)) => {
                 // println!("socket addr: {:?}", addr);
 
+                let db = Arc::clone(&db);
+
                 tokio::spawn(async move {
-                    handle_stream(stream).await;
+                    handle_stream(stream, db).await;
                 });
             }
             Err(e) => println!("couldn't get client: {:?}", e),
@@ -20,7 +29,7 @@ async fn main() {
     }
 }
 
-async fn handle_stream(stream: TcpStream) {
+async fn handle_stream(stream: TcpStream, db: Db) {
     let (rd, mut wr) = stream.into_split();
     let mut rd = BufReader::new(rd);
 
@@ -42,7 +51,26 @@ async fn handle_stream(stream: TcpStream) {
                         let _ = wr.write_all(b"+PONG\r\n").await;
                     }
                     [cmd, arg] if cmd.to_uppercase() == "ECHO".to_string() => {
-                        let response = encode_bulk_strings(arg.clone());
+                        let _ = wr.write_all(encode_bulk_strings(arg.clone()).as_bytes()).await;
+                    }
+                    [cmd, arg1, arg2] if cmd.to_uppercase() == "SET".to_string() => {
+                        {
+                            let mut db = db.lock().unwrap();
+                            db.insert(arg1.to_string(), arg2.to_string());
+                        }
+                        let _ = wr.write_all(encode_simple_strings("OK".to_string()).as_bytes()).await;
+                    }
+                    [cmd, arg] if cmd.to_uppercase() == "GET".to_string() => {
+                        let response = {
+                            let db = db.lock().unwrap();
+                            
+                            if let Some(value) = db.get(arg) {
+                                encode_bulk_strings(value.clone())
+                            } else {
+                                encode_bulk_strings("".to_string())
+                            }
+                        };
+
                         let _ = wr.write_all(response.as_bytes()).await;
                     }
                     _ => unreachable!(),
@@ -51,6 +79,26 @@ async fn handle_stream(stream: TcpStream) {
             Err(_) => break,
         }
     }
+}
+
+
+/// Simple strings 
+/// 
+/// Simple strings are encoded as a plus (+) character, followed by a string. The string mustn't contain a CR (\r) or LF (\n) character and is terminated by CRLF (i.e., \r\n).
+/// Simple strings transmit short, non-binary strings with minimal overhead. For example, many Redis commands reply with just "OK" on success. 
+/// The encoding of this Simple String is the following 5 bytes:
+/// ```text
+/// +OK\r\n
+/// ```
+/// When Redis replies with a simple string, a client library should return to the caller a string value composed of the first character after the + up to the end of the string, excluding the final CRLF bytes.
+/// To send binary strings, use bulk strings instead.
+fn encode_simple_strings(input: String) -> String {
+    format!("+{}\r\n", input)
+}
+
+/// RESP decode simple strings
+fn decode_simple_strings(input: String) -> String {
+    input.trim_end_matches("\r\n").trim_start_matches("+").to_string()
 }
 
 /// RESP encode bulk strings
@@ -77,12 +125,16 @@ async fn handle_stream(stream: TcpStream) {
 /// $5\r\nhello\r\n
 /// ```
 fn encode_bulk_strings(input: String) -> String {
-    format!("${}\r\n{}\r\n", input.len(), input)
+    if input.is_empty() {
+        format!("$-1\r\n")
+    } else {
+        format!("${}\r\n{}\r\n", input.len(), input)
+    }
 }
 
 /// RESP decode bulk strings
 fn decode_bulk_strings(input: String) -> String {
-    input.split_ascii_whitespace().nth(1).unwrap().to_string()
+    todo!()
 }
 
 /// RESP encode arrays'
